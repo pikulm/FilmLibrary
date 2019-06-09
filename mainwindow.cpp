9c7078a400a9775e176a1b3d2af75825e4689d09
Magdalena Pikul <magdalena.anna.pikul@gmail.com>
2019-06-09 20:36:20 +0200

    Added first buttons to the main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+#include <QPixmap>
+
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->searchButton->setStyleSheet("QPushButton {color: #df0ac9}");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/**
+ * @brief MainWindow::doWhenSearchButtonPressed ask the OMDb API for details
+ * about a film specified by a title and, opionally, year of release.
+ *
+ * Read the title from the "titleEdit" button and, optionally, the year of
+ * release from the "yearEdit" button. Then, connect with the OMDb API and
+ * fetch the data.
+ *
+ * @param checked true if the button is checked, or false if the button is unchecked
+ */
+void MainWindow::doWhenSearchButtonPressed()
+{
+    QString title = ui->titleEdit->text();
+    ui->resultText->setText(title);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,6 +16,9 @@ public:
 
 private:
     Ui::MainWindow* ui;
+
+private slots:
+    void doWhenSearchButtonPressed();
 };
 
 #endif // MAINWINDOW_H
